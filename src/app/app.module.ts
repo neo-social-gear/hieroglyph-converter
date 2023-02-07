40c8b70ae0b7a6df71e6cf4675be61d80be6bbae
Tetsuya-Minase <tetsuya.minasen@gmail.com>
2023-02-07 20:57:12 +0900

    feat: add header component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { HeaderModule } from './components/headers/header.module';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, HeaderModule],
   providers: [],
   bootstrap: [AppComponent],
 })
